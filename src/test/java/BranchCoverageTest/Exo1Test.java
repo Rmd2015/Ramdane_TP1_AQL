46c383cd2c85af5d0c76f79b4d6344f87653c229
Rmd2015 <ibad.ram1@gmail.com>
2024-02-27 10:32:55 +0100

    BRANCHE COVERAGE EX01

--- src/test/java/BranchCoverageTest/Exo1Test.java
+++ src/test/java/BranchCoverageTest/Exo1Test.java
@@ -5,6 +5,11 @@ import org.example.Exo1.PalindromeCorrection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class Exo1Test {
-  
+  @Test
+    public void PalidromeBranchCoverageTest()
+  {
+      //COVERTURE DE BRANCAHE ON NE RENTRE PAS DANS LE DETAIILES.
+      Assertions.assertTrue(PalindromeCorrection.isPalindrome("rar"));
+  }
 
 }
